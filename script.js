cc8a9f8dcfa7c03958aa9ada2b40c8b57fca9fe2
Veronica Ulrikkeholm <mail@veronicaisabelle.com>
2022-09-01 12:33:42 +0200

    added selected class properly

--- script.js
+++ script.js
@@ -8,45 +8,13 @@ let selected = "";
 
 buttons.forEach(btn => {
     btn.addEventListener("click", (e) => {
-        removeSelectedClasses()
         selected = e.target.textContent
         e.target.classList.add("selected")
     })
 })
 
 submit.addEventListener("click", (e) => {
-    if (selected === "") {
-        addButtonErrors()
-        setTimeout(() => {
-            removeButtonErrors()
-        }, 3000);
-        return
-    }
-
     selectionMsg.textContent = `You selected ${selected} out of 5`
     box1.classList.add("d-none");
     box2.classList.remove("d-none");
 })
-
-function addButtonErrors()
-{
-    error.classList.remove("d-none")
-    buttons.forEach(btn => {
-        btn.classList.add("error")
-    })
-}
-
-function removeButtonErrors()
-{
-    error.classList.add("d-none")
-    buttons.forEach(btn => {
-        btn.classList.remove("error")
-    })
-}
-
-function removeSelectedClasses()
-{
-    buttons.forEach(btn => {
-        btn.classList.remove("selected")
-    })
-}
